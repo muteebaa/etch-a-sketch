3b0b2a19914b90ad1dd1c39dc4f79f3fe87a34fb
muteebaa <muteeba.jamal02@gmail.com>
2022-08-14 22:38:47 +0500

    Added button to prompt user to change grid size

--- js-file.js
+++ js-file.js
@@ -4,26 +4,56 @@ const container = document.querySelector('#container');
 
 
 // creating grid
-function createGrid(){
-    for (let i = 0; i < 256; i++){
+function createGrid(size){
+    console.log(`create grid : ${size}`);
+    for (let i = 0; i < size; i++){
         const square = document.createElement('div');
         square.classList.add('square');
 
-
         container.appendChild(square);
+    }
+    
+    container.style["grid-template-columns"] = `repeat(${Math.sqrt(size)}, auto)`;
+    changeColour();
+}
+
+function gridSize(){
+    let size = prompt("Please enter width");
+    
+    if (size != null) {
+       console.log(size);
+
+
+       while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+
+        createGrid(Math.pow(size,2));
     }
 }
 
-createGrid();
-
 // change colour upon hover
-const squares = document.querySelectorAll('.square');
+function changeColour(){
+    const squares = document.querySelectorAll('.square');
 
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.classList.add('hover');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hover');
+        });
     });
-});
+}
+
+createGrid(Math.pow(16,2));
+
+// button to prompt user for grid size
+const body = document.querySelector('body');
+
+const button = document.createElement('button');
+button.textContent = "Select grid size";
+body.appendChild(button);
+
+button.addEventListener('click', gridSize);
+
 
 
 
